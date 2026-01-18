052d9768b8b086ad6a0670990c80aa5fd3825341
plantationandcooperation-project <plantationandcooperation@gmail.com>
2026-01-19 00:26:30 +0530

    falling tree leaps

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const container = document.querySelector(".leaves-container");
 
+function createLeaf() {
+  const leaf = document.createElement("div");
+  leaf.classList.add("leaf");
+  leaf.innerHTML = "🍃";
+
+  leaf.style.left = Math.random() * 100 + "vw";
+  leaf.style.animationDuration = 5 + Math.random() * 5 + "s";
+  leaf.style.fontSize = 16 + Math.random() * 20 + "px";
+
+  container.appendChild(leaf);
+
+  setTimeout(() => {
+    leaf.remove();
+  }, 10000);
+}
+
+setInterval(createLeaf, 400);
