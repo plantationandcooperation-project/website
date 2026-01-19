f0248abc494e16f721c447714a5b776c04667509
plantationandcooperation-project <plantationandcooperation@gmail.com>
2026-01-19 23:41:10 +0530

    js bug fix

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 alert("JS Loaded");
+
 const container = document.querySelector(".leaves-container");
 
-function createLeaf() {
-  const leaf = document.createElement("div");
-  leaf.classList.add("leaf");
-  leaf.innerHTML = "🍃";
+if (container) {
+  function createLeaf() {
+    const leaf = document.createElement("div");
+    leaf.classList.add("leaf");
+    leaf.innerHTML = "🍃";
 
-  leaf.style.left = Math.random() * 100 + "vw";
-  leaf.style.animationDuration = 5 + Math.random() * 5 + "s";
-  leaf.style.fontSize = 16 + Math.random() * 20 + "px";
+    leaf.style.left = Math.random() * 100 + "vw";
+    leaf.style.animationDuration = 5 + Math.random() * 5 + "s";
+    leaf.style.fontSize = 16 + Math.random() * 20 + "px";
 
-  container.appendChild(leaf);
+    container.appendChild(leaf);
 
-  setTimeout(() => {
-    leaf.remove();
-  }, 10000);
+    setTimeout(() => {
+      leaf.remove();
+    }, 10000);
+  }
+
+  setInterval(createLeaf, 400);
 }
 
-setInterval(createLeaf, 400);
 
 let pledgeBtn = document.getElementById("pledgeBtn");
 let pledgeCountText = document.getElementById("pledgeCount");
